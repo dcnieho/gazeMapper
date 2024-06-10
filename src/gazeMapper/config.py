7ba574e9220af3901154f82aa5029207a099f251
Diederick Niehorster <dcnieho@gmail.com>
2024-06-10 16:38:16 +0200

    config: now supports configuring individual markers to also be detected and reported

--- src/gazeMapper/config.py
+++ src/gazeMapper/config.py
@@ -1,19 +1,33 @@
 import pathlib
 import json
+from typing import Any
 
 from glassesTools import utils
 
 from . import episode, plane, session
 
 
+class Marker:
+    def __init__(self, id:int, size:float):
+        self.id     = id
+        self.size   = size
+utils.register_type(utils.CustomTypeEntry(Marker,'__config.Marker__',lambda x: {'id': x.id, 'size': x.size}, lambda x: Marker(**x)))
+
+def get_marker_dict_from_list(markers: list[Marker]) -> dict[int,dict[str,Any]]:
+    out = {}
+    for m in markers:
+        out[m.id] = {'marker_size': m.size}
+    return out
+
 class Study:
     default_json_file_name = 'study_def.json'
 
-    def __init__(self, session_def: session.SessionDefinition, planes: list[plane.Definition], planes_per_interval: dict[episode.Event,list[str]], working_directory: str|pathlib.Path):
+    def __init__(self, session_def: session.SessionDefinition, planes: list[plane.Definition], planes_per_interval: dict[episode.Event,list[str]], individual_markers: list[Marker], working_directory: str|pathlib.Path):
         self.session_def        = session_def
         self.planes             = planes
         self.planes_per_interval= planes_per_interval
         self.working_directory  = working_directory
+        self.individual_markers = individual_markers
 
         self._check_planes_per_interval()
 
@@ -29,8 +43,8 @@ class Study:
         # instead to remain flexible and make it easy for users to rename, etc
         d_path = path / self.default_json_file_name
         with open(d_path, 'w') as f:
-            to_dump = {k:getattr(self,k) for k in ['planes_per_interval']}    # only this field. Name will be populated from name of session/provided folder, recordings from each subfolder in the session/provided folder, and working_directory as the provided path
-            to_dump['planes_per_interval'] = [(k, to_dump['planes_per_interval'][k]) for k in to_dump['planes_per_interval']]
+            to_dump = {k:getattr(self,k) for k in ['planes_per_interval','individual_markers']}    # only these fields. Name will be populated from name of session/provided folder, recordings from each subfolder in the session/provided folder, and working_directory as the provided path
+            to_dump['planes_per_interval'] = [(k, to_dump['planes_per_interval'][k]) for k in to_dump['planes_per_interval']]   # pack as list of tuples for storage
             # dump to file
             json.dump(to_dump, f, cls=utils.CustomTypeEncoder, indent=2)
         # this doesn't story any files itself, but triggers the contained info to be stored
@@ -48,7 +62,7 @@ class Study:
         d_path = path / Study.default_json_file_name
         with open(d_path, 'r') as f:
             kwds = json.load(f, object_hook=utils.json_reconstitute)
-        kwds['planes_per_interval'] = {k:v for k,v in kwds['planes_per_interval']}
+        kwds['planes_per_interval'] = {k:v for k,v in kwds['planes_per_interval']}  # stored as list of tuples, unpack
         # get session def
         s_path = path / 'session_def.json'
         if not s_path.is_file():
