35dcc2a28d78ecd801cfdcace6dd13732a3d40e1
Diederick Niehorster <dcnieho@gmail.com>
2024-06-17 16:46:30 +0200

    update (my own) testing code

--- test.py
+++ test.py
@@ -1,36 +1,61 @@
 import pathlib
 
-from glassesTools import eyetracker, importing
+from glassesTools import eyetracker, importing, marker
 
-from src.gazeMapper import camera_recording, episode, session
+from src.gazeMapper import camera_recording, config, episode, session, plane
 from src.gazeMapper.process.code_episodes import process as do_coding
+from src.gazeMapper.process.detect_markers import process as detect_markers
+from src.gazeMapper.process.sync_VOR import process as sync_VOR
+from src.gazeMapper.process.sync_to_ref import process as sync_to_ref
 
 
 base = pathlib.Path(r'C:\dat\projects\Roy Japanese Lego\pilot 3\data\J00')
 proj = pathlib.Path(r'C:\dat\projects\gazeMapper\test_proj')
 
-# sess_def = session.SessionDefinition.load_from_json(proj/'config'/'session.json')
-
-# sess_def.store_as_json(proj/'session_test.json')
-
 
 # # create session
-# sess = session.Session(sess_def,'test1')
+# sess_def = session.SessionDefinition.load_from_json(proj/'config'/'session_def.json')
+# sess = session.Session(sess_def,'N24')
 # sess.create_working_directory(proj)
 # # import recordings
-# rec_info = importing.get_recording_info(base/'J00A'/'20230518T012901Z', eyetracker.EyeTracker.Tobii_Glasses_3)
-# rec = sess.import_and_add_recording('test_et1',rec_info[0])
+# #rec_info = importing.get_recording_info(base/'J13A'/'20231019T081115Z', eyetracker.EyeTracker.Tobii_Glasses_3)
+# rec_info = importing.get_recording_info(base/'N24A'/'2023-11-30_10-10-47-d1aa2982', eyetracker.EyeTracker.Pupil_Invisible)
+# rec = sess.import_and_add_recording('et1',rec_info[0],False)
 
-# rec_info = importing.get_recording_info(base/'J00D'/'20230518T012502Z', eyetracker.EyeTracker.Tobii_Glasses_3)
-# rec = sess.import_and_add_recording('test_et2',rec_info[0])
+# #rec_info = importing.get_recording_info(base/'J13D'/'20231019T081031Z', eyetracker.EyeTracker.Tobii_Glasses_3)
+# rec_info = importing.get_recording_info(base/'N24D'/'2023-11-30_10-10-31-cc2e58e9', eyetracker.EyeTracker.Pupil_Invisible)
+# rec = sess.import_and_add_recording('et2',rec_info[0],False)
 
-# rec_info = camera_recording.Recording('topview_2023-05-18_10-32-48','topview_2023-05-18_10-32-48.mp4', base)
-# rec = sess.import_and_add_recording('test_cam',rec_info)
+# #rec_info = camera_recording.Recording('topview','2023-10-19_17-14-59.mp4', base)
+# rec_info = camera_recording.Recording('topview','2023-11-30_10-13-09.mov', base)
+# rec = sess.import_and_add_recording('cam',rec_info,False)
 
 # sess.store_as_json()
 
+
+# sd = session.SessionDefinition.load_from_json(proj/'config'/'session_def.json')
+# st = config.Study.load_from_json(proj/'config')
+
+# sess = session.Session.from_definition(st.session_def,proj/'test1')
+# sess.has_all_recordings()
+
+# sess2 = session.Session.load_from_json(proj/'test1')
+
+# pl1 = plane.get_plane_from_definition(st.planes[0], proj/'config'/st.planes[0].name)
+# pl2 = plane.get_plane_from_path(proj/'config'/st.planes[1].name)
+
 sessions = session.get_sessions_from_directory(proj)
 
+# do_coding(sessions[0].recordings['et1'].info.working_directory,session.RecordingType.EyeTracker)
+# do_coding(sessions[0].recordings['et2'].info.working_directory,session.RecordingType.EyeTracker)
+# do_coding(sessions[0].recordings['cam'].info.working_directory,session.RecordingType.Camera)
 
-#do_coding(sessions[0].recordings['test_et1'].info.working_directory,'None',session.RecordingType.EyeTracker)
-do_coding(sessions[0].recordings['test_cam'].info.working_directory,'None',session.RecordingType.Camera)
+# for s in sessions:
+#     detect_markers(s.recordings['cam'].info.working_directory,proj/'config',session.RecordingType.Camera)
+#     detect_markers(s.recordings['et1'].info.working_directory,proj/'config',session.RecordingType.EyeTracker)
+#     detect_markers(s.recordings['et2'].info.working_directory,proj/'config',session.RecordingType.EyeTracker)
+
+for s in sessions:
+    sync_VOR(s.recordings['et1'].info.working_directory,proj/'config')
+    sync_VOR(s.recordings['et2'].info.working_directory,proj/'config')
+    sync_to_ref(s.working_directory,proj/'config',do_time_stretch=True)
