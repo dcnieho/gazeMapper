cdc46fa552193fd68ef0c047b45c4dd1169dec37
Diederick Niehorster <dcnieho@gmail.com>
2024-08-01 23:11:54 +0200

    rename marker json storage identifier

--- src/gazeMapper/marker.py
+++ src/gazeMapper/marker.py
@@ -14,7 +14,7 @@ class Marker:
     def __init__(self, id:int, size:float):
         self.id     = id
         self.size   = size
-utils.register_type(utils.CustomTypeEntry(Marker,'__config.Marker__',lambda x: {'id': x.id, 'size': x.size}, lambda x: Marker(**x)))
+utils.register_type(utils.CustomTypeEntry(Marker,'__marker.Marker__',lambda x: {'id': x.id, 'size': x.size}, lambda x: Marker(**x)))
 
 def get_marker_dict_from_list(markers: list[Marker]) -> dict[int,dict[str]]:
     out = {}
