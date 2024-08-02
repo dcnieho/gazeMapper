9a24cc3270b9c151025161a5852b45ae9124b80b
Diederick Niehorster <dcnieho@gmail.com>
2024-08-03 00:58:41 +0200

    settings_editor: now have an add item button for dicts that are not TypedDicts (does nothing yet)

--- src/gazeMapper/GUI/_impl/settings_editor.py
+++ src/gazeMapper/GUI/_impl/settings_editor.py
@@ -63,7 +63,7 @@ def _get_field_type(field: str, obj: _T, f_type: typing.Type, possible_value_get
 
 def _draw_impl(obj: _C, fields: list[str], types: dict[str, typing.Type], defaults: dict[str, typing.Any], possible_value_getters: dict[str, typing.Callable[[_C], tuple[typing.Any]]], level=0, table_is_started=False) -> tuple[bool,bool]:
     changed = False
-    max_fields_width = max([imgui.calc_text_size(f) for f in fields], key=lambda x: x.x, default=imgui.calc_text_size('xxxxx')).x*1.1   # 10% extra to be safe
+    max_fields_width = _get_fields_text_width(fields)*1.1   # 10% extra to be safe
     for f in fields:
         is_dict, base_type, f_type = _get_field_type(f, obj, types[f], possible_value_getters[f] if f in possible_value_getters else None)
 
@@ -72,7 +72,7 @@ def _draw_impl(obj: _C, fields: list[str], types: dict[str, typing.Type], defaul
                 imgui.end_table()
                 table_is_started = False
             if imgui.tree_node_ex(f,imgui.TreeNodeFlags_.framed):
-                this_changed, made_obj, new_sub_obj = _draw_dict_editor(obj.get(f,None) if isinstance(obj,dict) else getattr(obj,f), f_type)
+                this_changed, made_obj, new_sub_obj = _draw_dict_editor(obj.get(f,None) if isinstance(obj,dict) else getattr(obj,f), f_type, level+1)
                 changed |= this_changed
                 if this_changed and made_obj:
                     setattr(obj,f,new_sub_obj)
@@ -81,16 +81,14 @@ def _draw_impl(obj: _C, fields: list[str], types: dict[str, typing.Type], defaul
 
         # simple field, set up for drawing
         if not table_is_started:
-            table_is_started = imgui.begin_table(f"##settings_level_{level}", 2)
+            table_is_started = _start_table(level, max_fields_width)
             if not table_is_started:
                 continue
-            imgui.table_setup_column("setting", imgui.TableColumnFlags_.width_fixed, init_width_or_weight=max_fields_width)
-            imgui.table_setup_column("value", imgui.TableColumnFlags_.width_stretch)
 
         changed |= _draw_field(f, obj, base_type, f_type, defaults[f] if f in defaults else None)
     return table_is_started, changed
 
-def _draw_dict_editor(obj: _T, o_type: typing.Type):
+def _draw_dict_editor(obj: _T, o_type: typing.Type, level: int):
     if (made_obj := obj is None):
         obj = o_type()
 
@@ -103,12 +101,34 @@ def _draw_dict_editor(obj: _T, o_type: typing.Type):
         fields = list(obj.keys())
         types = {k:type(obj[k]) for k in obj}
 
-    table_is_started, changed = _draw_impl(obj, fields, types, {}, {})
+    first_column_width = _get_fields_text_width(fields, backup_str='xadd itemx')*1.1
+    table_is_started = _start_table(level, first_column_width)
+    if not table_is_started:
+        return
+    table_is_started, changed = _draw_impl(obj, fields, types, {}, {}, level, table_is_started)
+    if not table_is_started and has_add_remove:
+        table_is_started = _start_table(level, first_column_width)
+        if not table_is_started:
+            return
+    if has_add_remove:
+        imgui.table_next_row()
+        imgui.table_next_column()
+        imgui.button('add item')
     if table_is_started:
         imgui.end_table()
 
     return changed, made_obj, obj
 
+def _get_fields_text_width(fields, backup_str='xxxxx'):
+    return max([imgui.calc_text_size(f) for f in fields], key=lambda x: x.x, default=imgui.calc_text_size(backup_str)).x
+
+def _start_table(level, first_column_width):
+    table_is_started = imgui.begin_table(f"##settings_level_{level}", 2)
+    if not table_is_started:
+        return table_is_started
+    imgui.table_setup_column("setting", imgui.TableColumnFlags_.width_fixed, init_width_or_weight=first_column_width)
+    imgui.table_setup_column("value", imgui.TableColumnFlags_.width_stretch)
+    return table_is_started
 
 def _draw_field(field: str, obj: _T, base_type: typing.Type, f_type: typing.Type, default: _T|None) -> bool:
     imgui.table_next_row()
