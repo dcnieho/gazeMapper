11a079f721b780bc4eec174ac5558812ca2b66eb
Diederick Niehorster <dcnieho@gmail.com>
2024-08-02 14:16:29 +0200

    settings_editor: can now draw simple values, added typing.Literal

--- src/gazeMapper/GUI/_impl/settings_editor.py
+++ src/gazeMapper/GUI/_impl/settings_editor.py
@@ -11,25 +11,60 @@ class SettingEditor:
         self.types      = types
         self.defaults   = defaults
 
-    def _draw_impl(self, field: str, obj: typing.Any, f_type: typing.Type, default: typing.Any|None):
-        match typing.get_origin(f_type) or f_type:  # for instance str[int]->str, and or for str->str
-            case builtins.bool:
-                imgui.text(f'{field} is a bool')
-            case builtins.str:
-                imgui.text(f'{field} is a str')
-            case builtins.int:
-                imgui.text(f'{field} is an int')
-            case builtins.float:
-                imgui.text(f'{field} is a float')
-            case td if typing.is_typeddict(f_type):
-                imgui.text(f'{field} is a TypedDict')
-            case builtins.dict:
-                imgui.text(f'{field} is a dict')
-            case builtins.list:
-                imgui.text(f'{field} is a list')
+    def _draw_impl(self, table_is_started = False):
+        for f in self.fields:
+            table_is_started = self._draw_field(f, self.obj, self.types[f], self.defaults[f] if f in self.defaults else None, table_is_started)
+        return table_is_started
+
+    def _draw_field(self, field: str, obj: typing.Any, f_type: typing.Type, default: typing.Any|None, table_is_started: bool):
+        base_type = typing.get_origin(f_type) or f_type  # for instance str[int]->str, and or for str->str
+        match base_type:
+            case builtins.bool | builtins.str | builtins.int | builtins.float | typing.Literal:
+                is_container = False
+            case _ if typing.is_typeddict(f_type):
+                is_container = True
+            case builtins.dict | builtins.list:
+                is_container = True
             case _:
-                imgui.text(f'{field} ({f_type}) unmatched')
+                raise ValueError(f'type of {field} ({f_type}) not handled')
+
+        if is_container:
+            # recurse
+            return table_is_started
+
+        # simple field, draw
+        if not table_is_started:
+            table_is_started = imgui.begin_table("some_name", 2)
+            if not table_is_started:
+                return table_is_started
+            imgui.table_setup_column("setting", imgui.TableColumnFlags_.width_fixed)
+            imgui.table_setup_column("value", imgui.TableColumnFlags_.width_stretch)
+
+        imgui.table_next_row()
+        imgui.table_next_column()
+        imgui.align_text_to_frame_padding()
+        imgui.text(field)
+        imgui.table_next_column()
+        match base_type:
+            case builtins.bool:
+                setattr(obj,field,imgui.checkbox(f'##{field}', getattr(obj,field))[1])
+            case builtins.str:
+                setattr(obj,field,imgui.input_text(f'##{field}', getattr(obj,field))[1])
+            case builtins.int:
+                setattr(obj,field,imgui.input_int(f'##{field}', getattr(obj,field))[1])
+            case builtins.float:
+                setattr(obj,field,imgui.input_double(f'##{field}', getattr(obj,field))[1])
+            case typing.Literal:
+                values = typing.get_args(f_type)
+                p_idx = values.index(getattr(obj,field))
+                _,p_idx = imgui.combo(f"##{field}", p_idx, values, popup_max_height_in_items=min(10,len(values)))
+                setattr(obj,field,values[p_idx])
+
+        return table_is_started
 
     def draw(self):
-        for f in self.fields:
-            self._draw_impl(f, self.obj, self.types[f], self.defaults[f] if f in self.defaults else None)
+        if not self.fields:
+            return
+
+        if self._draw_impl():
+            imgui.end_table()
