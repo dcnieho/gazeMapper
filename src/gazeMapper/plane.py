b31c1f5d38c6516dba425c6b0fd7c56e6b695b46
Diederick Niehorster <dcnieho@gmail.com>
2024-08-03 15:41:06 +0200

    plane: proper aruco dict typing

--- src/gazeMapper/plane.py
+++ src/gazeMapper/plane.py
@@ -17,6 +17,9 @@ class Type(utils.AutoName):
 utils.register_type(utils.CustomTypeEntry(Type,'__enum.plane.Type__',str, lambda x: getattr(Type, x.split('.')[1])))
 types = [p for p in Type]
 
+aruco_dicts_to_str = {getattr(cv2.aruco,k):k for k in ['DICT_4X4_50', 'DICT_4X4_100', 'DICT_4X4_250', 'DICT_4X4_1000', 'DICT_5X5_50', 'DICT_5X5_100', 'DICT_5X5_250', 'DICT_5X5_1000', 'DICT_6X6_50', 'DICT_6X6_100', 'DICT_6X6_250', 'DICT_6X6_1000', 'DICT_7X7_50', 'DICT_7X7_100', 'DICT_7X7_250', 'DICT_7X7_1000', 'DICT_ARUCO_ORIGINAL', 'DICT_APRILTAG_16H5', 'DICT_APRILTAG_25H9', 'DICT_APRILTAG_36H10', 'DICT_APRILTAG_36H11', 'DICT_ARUCO_MIP_36H12']}
+ArucoDictType = typing.Literal[tuple(aruco_dicts_to_str.keys())]
+
 class Definition:
     default_json_file_name = 'plane_def.json'
 
@@ -32,7 +35,7 @@ class Definition:
                  plane_size         : plane.Coordinate|None     = None,
                  origin             : plane.Coordinate|None     = None,
                  unit               : str|None                  = None,
-                 aruco_dict         : int|None                  = None,
+                 aruco_dict         : ArucoDictType|None        = None,
                  ref_image_size     : int|None                  = None
                  ):
         self.type               = type
