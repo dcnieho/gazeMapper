aead173d0605d7995f054658cf68e155bb9698d7
Diederick Niehorster <dcnieho@gmail.com>
2024-08-08 10:27:18 +0200

    settings_editor dict editor: useful error popups for missing fields

--- src/gazeMapper/GUI/_impl/settings_editor.py
+++ src/gazeMapper/GUI/_impl/settings_editor.py
@@ -179,13 +179,13 @@ def draw_dict_editor(obj: _T, o_type: typing.Type, level: int, fields: list=None
         types = o_type.__annotations__
         fields = list(types.keys())
         if not mark:
-            mark = {k:None for k in o_type.__required_keys__ if k not in obj}
+            mark = {k:f'{k} is required' for k in o_type.__required_keys__ if k not in obj}
     elif typed_dict_defaults.is_typeddictdefault(o_type):
         types = o_type.__annotations__
         fields = list(types.keys())
         defaults = o_type._field_defaults.copy()
         if not mark:
-            mark = {k:None for k in o_type.__required_keys__ if k not in obj}
+            mark = {k:f'{k} is required' for k in o_type.__required_keys__ if k not in obj}
     elif is_NamedTuple_type(o_type):
         types = o_type.__annotations__
         fields= list(o_type._fields)
