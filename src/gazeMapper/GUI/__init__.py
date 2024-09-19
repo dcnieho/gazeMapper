275d404a29991a5700094c3ee6b0bd2ab79d1d3e
Diederick Niehorster <dcnieho@gmail.com>
2024-09-19 16:32:14 +0200

    GUI/__init__.py: remove duplicate code

--- src/gazeMapper/GUI/__init__.py
+++ src/gazeMapper/GUI/__init__.py
@@ -1,26 +1,7 @@
-import enum
-import sys
-
-from glassesTools import async_thread
+from glassesTools import async_thread, platform
 
 from ._impl.gui import GUI as _GUI
 
-
-class Os(enum.Enum):
-    Windows = enum.auto()
-    MacOS   = enum.auto()
-    Linux   = enum.auto()
-
-if sys.platform.startswith("win"):
-    os = Os.Windows
-elif sys.platform.startswith("linux"):
-    os = Os.Linux
-elif sys.platform.startswith("darwin"):
-    os = Os.MacOS
-else:
-    print("Your system is not officially supported at the moment!\n"
-          "You can let me know on GitHub, or you can try porting yourself ;)")
-    sys.exit(1)
 
 # GUI requires some setup, call these functions
 def set_up():
@@ -29,7 +10,7 @@ def clean_up():
     async_thread.cleanup()
 
 def run():
-    if os!=Os.Windows:
+    if platform.os!=platform.Os.Windows:
         # Install uvloop on MacOS and Linux
         try:
             import uvloop
