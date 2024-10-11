93005ef8de6f80b37e3f1ec1e7fb77ab92ae6506
Diederick Niehorster <dcnieho@gmail.com>
2024-10-11 15:45:13 +0200

    improve hover tooltip for actions in context menu in gui

--- src/gazeMapper/GUI/_impl/gui.py
+++ src/gazeMapper/GUI/_impl/gui.py
@@ -1159,8 +1159,9 @@ class GUI:
                 hover_text = f'Run {a.displayable_name} for session: {session_name}'
                 status = self.sessions[session_name].state[a]
             else:
-                hover_text = f'Run {a.displayable_name} for recordings:\n'+'\n'.join(actions[a])
+                hover_text = f'Run {a.displayable_name} for recordings:\n- '+'\n- '.join(actions[a])
                 status = max([self.sessions[session_name].recordings[r].state[a] for r in actions[a]])
+            hover_text = hover_text.replace('Run Run','Run')
             icon = ifa6.ICON_FA_PLAY if status<process.State.Completed else ifa6.ICON_FA_ARROW_ROTATE_RIGHT
             if imgui.selectable(icon+f" {a.displayable_name}##{session_name}", False)[0]:
                 if process.is_session_level_action(a):
