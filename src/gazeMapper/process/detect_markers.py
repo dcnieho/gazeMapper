45de0c0babb50109ac38e6ec68fd6b74b9d97d7a
Diederick Niehorster <dcnieho@gmail.com>
2024-11-18 23:05:04 +0100

    detect_markers: rename missing_coding_ok to has_auto_coding as that is a more generic name useful in more situations

--- src/gazeMapper/process/detect_markers.py
+++ src/gazeMapper/process/detect_markers.py
@@ -46,12 +46,12 @@ def do_the_work(working_dir: pathlib.Path, config_dir: pathlib.Path, gui: GUI, v
 
     # get interval(s) coded to be analyzed, if any
     # We don't need them if they would be ignored because the whole video would be processed. The whole video is processed when study_config.auto_code_sync_points or study_config.auto_code_trial_episodes are set
-    missing_coding_ok = not not study_config.auto_code_sync_points or not not study_config.auto_code_trial_episodes
+    has_auto_code = not not study_config.auto_code_sync_points or not not study_config.auto_code_trial_episodes
     episode_file = working_dir / naming.coding_file
     if episode_file.is_file():
         episodes = episode.list_to_marker_dict(episode.read_list_from_file(), study_config.episodes_to_code)
     else:
-        if not missing_coding_ok:
+        if not has_auto_code:   # missing coding is ok when auto coding is set up, as then we process all frames anyway
             raise RuntimeError(f'Coding is missing, cannot run Detect Markers\n{episode_file}')
         episodes = episode.get_empty_marker_dict(list(study_config.episodes_to_code))
 
@@ -62,7 +62,7 @@ def do_the_work(working_dir: pathlib.Path, config_dir: pathlib.Path, gui: GUI, v
         if annotation.Event.Trial in study_config.episodes_to_code:
             all_recs = [r.name for r in study_config.session_def.recordings]
             # NB: don't error if we don't need trial episodes for coding.
-            episodes[annotation.Event.Trial] = synchronization.get_episode_frame_indices_from_ref(working_dir, annotation.Event.Trial, rec_def.name, study_config.sync_ref_recording, all_recs, study_config.sync_ref_do_time_stretch, study_config.sync_ref_average_recordings, study_config.sync_ref_stretch_which, missing_ref_coding_ok=missing_coding_ok)
+            episodes[annotation.Event.Trial] = synchronization.get_episode_frame_indices_from_ref(working_dir, annotation.Event.Trial, rec_def.name, study_config.sync_ref_recording, all_recs, study_config.sync_ref_do_time_stretch, study_config.sync_ref_average_recordings, study_config.sync_ref_stretch_which, missing_ref_coding_ok=has_auto_code)
 
     sync_target_function         = _get_sync_function(study_config, rec_def, None if annotation.Event.Sync_ET_Data not in episodes else episodes[annotation.Event.Sync_ET_Data])
     planes_setup, analyze_frames = _get_plane_setup(study_config, config_dir, episodes)
