5f02094729f4938691b5c8e00121c6e028e2b135
Diederick Niehorster <dcnieho@gmail.com>
2025-04-05 10:26:42 +0200

    simplify main.py now no more freeze support

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
 #!/usr/bin/env python
-import sys
-import os
-import multiprocessing
-
-
-if getattr(sys, "frozen", False):
-    if not sys.platform.startswith("win"):
-        raise "Executable is only supported on Windows"
-
-    # need to call this so that code in __init__ of ffpyplayer
-    # doesn't encounter a None in site.USER_BASE
-    import site
-    site.getuserbase()
-
-    # need to put packaged ffmpeg binaries (dlls) on path
-    p = os.path.join(os.path.dirname(sys.executable),'lib')
-    os.environ["PATH"] = p + os.pathsep + os.environ["PATH"]
-    os.add_dll_directory(p)
-
 if __name__=="__main__":
-    multiprocessing.freeze_support()
-
     import gazeMapper.GUI
     gazeMapper.GUI.run()
