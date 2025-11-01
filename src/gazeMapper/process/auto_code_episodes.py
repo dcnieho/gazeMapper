6adb1300ebb92c18675469d1086a94b6daea7a43
Diederick Niehorster <dcnieho@gmail.com>
2025-11-01 17:43:36 +0100

    auto_code_episodes now works

--- src/gazeMapper/process/auto_code_episodes.py
+++ src/gazeMapper/process/auto_code_episodes.py
@@ -8,7 +8,7 @@ from glassesTools import annotation, marker as gt_marker, process_pool
 from .. import config, episode, naming, process, session
 
 
-def run(working_dir: str|pathlib.Path, config_dir: str|pathlib.Path = None, **study_settings):
+def run(working_dir: str|pathlib.Path, config_dir: str|pathlib.Path|None = None, **study_settings):
     working_dir = pathlib.Path(working_dir) # working directory of a session, not of a recording
     if config_dir is None:
         config_dir = config.guess_config_dir(working_dir)
@@ -17,57 +17,62 @@ def run(working_dir: str|pathlib.Path, config_dir: str|pathlib.Path = None, **st
 
     # get settings for the study
     study_config = config.read_study_config_with_overrides(config_dir, {config.OverrideLevel.Session: working_dir.parent, config.OverrideLevel.Recording: working_dir}, **study_settings)
-    if not study_config.auto_code_episodes:
-        raise ValueError(f'Nothing to do, no automatic episode coding is defined for this study')
-    rec_def = study_config.session_def.get_recording_def(working_dir.name)
+    sync_events: list[config.EventSetup] = []
+    for ev in [e for e in annotation.EventType if annotation.type_map[e]==annotation.Type.Interval]:
+        sync_events.extend(process.get_specific_event_types(study_config, ev, check_specific_fields=['auto_code']))
+    if not sync_events:
+        raise ValueError('No auto-coded event start and ends are configured for the study, nothing to process')
 
+    rec_def = study_config.session_def.get_recording_def(working_dir.name)
     if study_config.sync_ref_recording and rec_def.name!=study_config.sync_ref_recording:
-        # Trial events are only coded for the reference recording
-        study_config.auto_code_episodes.pop(annotation.EventType.Trial, None)
-        if not study_config.auto_code_episodes:
-            raise RuntimeError(f'Nothing to do, auto_code_episodes is defined only for trials and you have a sync_ref_recording ({study_config.sync_ref_recording}), but this recording ({rec_def.name}) is another one')
+        # Trial events are only coded for the reference recording, so they should be discarded here
+        sync_events = [cs for cs in sync_events if cs['event_type']!=annotation.EventType.Trial]
+        if not sync_events:
+            raise RuntimeError(f'Nothing to do, auto-coding of event start and ends is defined only for trial events and you have a sync_ref_recording ({study_config.sync_ref_recording}), but this recording ({rec_def.name}) is another one')
 
     # get already coded interval(s), if any
+    episodes_to_code = [cs['name'] for cs in sync_events]
     coding_file = working_dir / naming.coding_file
     if coding_file.is_file():
-        episodes = episode.list_to_marker_dict(episode.read_list_from_file(coding_file), study_config.episodes_to_code)
+        episodes = episode.list_to_marker_dict(episode.read_list_from_file(coding_file), episodes_to_code)
         # flatten
         for e in episodes:
             episodes[e] = [i for iv in episodes[e] for i in iv]
     else:
-        episodes = episode.get_empty_marker_dict(study_config.episodes_to_code)
+        episodes = episode.get_empty_marker_dict(episodes_to_code)
     episodes_original = copy.deepcopy(episodes)
 
     # get marker files
-    all_marker_ids = [study_config.auto_code_episodes[e]['start_markers']+study_config.auto_code_episodes[e]['end_markers'] for e in study_config.auto_code_episodes]
-    all_marker_ids = {m for ms in all_marker_ids for m in ms}
-    file_missing   = [not gt_marker.get_file_name(m.m_id, m.aruco_dict_id, working_dir).is_file() for m in all_marker_ids]
-    if any(file_missing):
-        file_missing = [gt_marker.get_file_name(m.m_id, m.aruco_dict_id, None) for m,miss in zip(all_marker_ids,file_missing) if miss]
-        missing_str  = '\n- '.join(file_missing)
-        raise FileNotFoundError(f'The following marker files were not found:\n- {missing_str}')
-    ori_markers = {m: gt_marker.read_dataframe_from_file(m.m_id, m.aruco_dict_id, working_dir) for m in all_marker_ids}
-    # recode so we have a boolean with when markers are present
-    ori_markers = {m: gt_marker.code_for_presence(ori_markers[m], allow_failed=True) for m in ori_markers if not ori_markers[m].empty}
-    # marker presence signal only contains marker detections (True). We need to fill the gaps in between detections with False (not detected) so we have a continuous signal without gaps
-    ori_markers = {m: gt_marker.expand_detection(ori_markers[m], fill_value=False) for m in ori_markers}
-    # now auto code indicated intervals
-    for e in study_config.auto_code_episodes:
-        markers = {m: copy.deepcopy(ori_markers[m]) for m in study_config.auto_code_episodes[e]['start_markers']+study_config.auto_code_episodes[e]['end_markers']}
+    for cs in sync_events:
+        for m in ('start_markers','end_markers'):
+            if m not in cs['auto_code'] or not cs['auto_code'][m]:
+                raise ValueError(f'No {m} configured for auto coding of sync event "{cs["name"]}"')
+        all_marker_ids = set(cs['auto_code']['start_markers']+cs['auto_code']['end_markers'])
+        file_missing   = [not gt_marker.get_file_name(m.m_id, m.aruco_dict_id, working_dir).is_file() for m in all_marker_ids]
+        if any(file_missing):
+            file_missing = [gt_marker.get_file_name(m.m_id, m.aruco_dict_id, None) for m,miss in zip(all_marker_ids,file_missing) if miss]
+            missing_str  = '\n- '.join(file_missing)
+            raise FileNotFoundError(f'The following marker files were not found:\n- {missing_str}')
+        markers = {m: gt_marker.read_dataframe_from_file(m.m_id, m.aruco_dict_id, working_dir) for m in all_marker_ids}
+        # recode so we have a boolean with when markers are present
+        markers = {m: gt_marker.code_for_presence(markers[m], allow_failed=True) for m in markers if not markers[m].empty}
+        # marker presence signal only contains marker detections (True). We need to fill the gaps in between detections with False (not detected) so we have a continuous signal without gaps
+        markers = {m: gt_marker.expand_detection(markers[m], fill_value=False) for m in markers}
+        # now auto code indicated intervals
         # see where stretches of marker presence start and end
         marker_starts: dict[gt_marker.MarkerID,list[int]] = {}
         marker_ends  : dict[gt_marker.MarkerID,list[int]] = {}
         for m in markers:
-            marker_starts[m], marker_ends[m] = gt_marker.get_appearance_starts_ends(markers[m], study_config.auto_code_episodes[e]['max_gap_duration'], study_config.auto_code_episodes[e]['min_duration'])
+            marker_starts[m], marker_ends[m] = gt_marker.get_appearance_starts_ends(markers[m], cs['auto_code']['max_gap_duration'], cs['auto_code']['min_duration'])
         # find potential interval starts and ends
-        if len(study_config.auto_code_episodes[e]['start_markers'])>1:
-            starts = gt_marker.get_sequence_interval(marker_starts, marker_ends, study_config.auto_code_episodes[e]['start_markers'], study_config.auto_code_episodes[e]['max_intermarker_gap_duration'], side='end')
+        if len(cs['auto_code']['start_markers'])>1:
+            starts = gt_marker.get_sequence_interval(marker_starts, marker_ends, cs['auto_code']['start_markers'], cs['auto_code']['max_intermarker_gap_duration'], side='end')
         else:
-            starts = marker_ends  [study_config.auto_code_episodes[e]['start_markers'][0]]
-        if len(study_config.auto_code_episodes[e][ 'end_markers' ])>1:
-            ends   = gt_marker.get_sequence_interval(marker_starts, marker_ends, study_config.auto_code_episodes[e][ 'end_markers' ], study_config.auto_code_episodes[e]['max_intermarker_gap_duration'], side='start')
+            starts = marker_ends  [cs['auto_code']['start_markers'][0]]
+        if len(cs['auto_code'][ 'end_markers' ])>1:
+            ends   = gt_marker.get_sequence_interval(marker_starts, marker_ends, cs['auto_code'][ 'end_markers' ], cs['auto_code']['max_intermarker_gap_duration'], side='start')
         else:
-            ends   = marker_starts[study_config.auto_code_episodes[e][ 'end_markers' ][0]]
+            ends   = marker_starts[cs['auto_code'][ 'end_markers' ][0]]
         # now match interval starts and ends
         # strategy: run through starts and find latest start that is before first end (discard ends that are before the start)
         # keep pointer into array keeping track of ends and start that have already been discarded or consumed
@@ -95,7 +100,9 @@ def run(working_dir: str|pathlib.Path, config_dir: str|pathlib.Path = None, **st
             s_idx+=mini+1
             e_idx+=1
         # now insert into coding file. This just overwrites whatever is there
-        episodes[e] = [y for x in intervals for y in x]
+        if cs['name'] not in episodes:
+            episodes[cs['name']] = []
+        episodes[cs['name']] = [y for x in intervals for y in x]
 
     # early exit if nothing has changed
     if episodes==episodes_original:
