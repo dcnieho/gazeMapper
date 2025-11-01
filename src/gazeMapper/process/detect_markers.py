42e9eef57c784975b536086153e4882eb5919325
Diederick Niehorster <dcnieho@gmail.com>
2025-11-01 12:36:11 +0100

    detect_markers now works

--- src/gazeMapper/process/detect_markers.py
+++ src/gazeMapper/process/detect_markers.py
@@ -1,7 +1,7 @@
 import pathlib
 import pandas as pd
 import numpy as np
-from typing import Any, Callable
+import typing
 
 from glassesTools import annotation, aruco, drawing, marker as gt_marker, naming as gt_naming, pose, process_pool, propagating_thread, ocv, timestamps
 from glassesTools.gui.video_player import GUI
@@ -9,7 +9,7 @@ from glassesTools.gui.video_player import GUI
 from .. import config, episode, marker, naming, plane, process, session, synchronization
 
 
-def run(working_dir: str|pathlib.Path, config_dir: str|pathlib.Path = None, show_visualization=False, visualization_show_rejected_markers=False, progress_indicator: process_pool.JobProgress=None, **study_settings):
+def run(working_dir: str|pathlib.Path, config_dir: str|pathlib.Path|None=None, show_visualization=False, visualization_show_rejected_markers=False, progress_indicator: process_pool.JobProgress|None=None, **study_settings):
     # if show_visualization, each frame is shown in a viewer, overlaid with info about detected markers and planes
     # if show_rejected_markers, rejected ArUco marker candidates are also shown in the viewer. Possibly useful for debug
     working_dir = pathlib.Path(working_dir)
@@ -37,7 +37,7 @@ def run(working_dir: str|pathlib.Path, config_dir: str|pathlib.Path = None, show
         do_the_work(working_dir, config_dir, None, False, progress_indicator, **study_settings)
 
 
-def do_the_work(working_dir: pathlib.Path, config_dir: pathlib.Path, gui: GUI, visualization_show_rejected_markers: bool, progress_indicator: process_pool.JobProgress, **study_settings):
+def do_the_work(working_dir: pathlib.Path, config_dir: pathlib.Path, gui: GUI|None, visualization_show_rejected_markers: bool, progress_indicator: process_pool.JobProgress|None, **study_settings):
     # progress indicator
     if progress_indicator is None:
         progress_indicator = process_pool.JobProgress(printer=lambda x: print(x))
@@ -53,26 +53,27 @@ def do_the_work(working_dir: pathlib.Path, config_dir: pathlib.Path, gui: GUI, v
 
     # get interval(s) coded to be analyzed, if any
     # We don't need them if they would be ignored because the whole video would be processed. The whole video is processed when study_config.auto_code_sync_points or study_config.auto_code_episodes are set
-    has_auto_code = not not study_config.auto_code_sync_points or not not study_config.auto_code_episodes
+    has_auto_code = process.config_has_auto_coding(study_config)
     episode_file = working_dir / naming.coding_file
     if episode_file.is_file():
-        episodes = episode.list_to_marker_dict(episode.read_list_from_file(episode_file), study_config.episodes_to_code)
+        episodes = episode.list_to_marker_dict(episode.read_list_from_file(episode_file), annotation.get_all_event_names())
     else:
         if not has_auto_code:   # missing coding is ok when auto coding is set up, as then we process all frames anyway
             raise RuntimeError(f'Coding is missing, cannot run Detect Markers\n{episode_file}')
-        episodes = episode.get_empty_marker_dict(list(study_config.episodes_to_code))
+        episodes = episode.get_empty_marker_dict(annotation.get_all_event_names())
 
     # trial episodes are gotten from the reference recording if there is one and this is not the reference recording
     if study_config.sync_ref_recording and rec_def.name!=study_config.sync_ref_recording:
-        if annotation.EventType.Trial in episodes and episodes[annotation.EventType.Trial]:
+        trial_events = process.get_specific_event_types(study_config, annotation.EventType.Trial)
+        if trial_events and any(episodes[e['name']] for e in trial_events):
             raise ValueError(f'Trial episodes are gotten from the reference recording ({study_config.sync_ref_recording}) and should not be coded for this recording ({rec_def.name})')
-        if annotation.EventType.Trial in study_config.episodes_to_code:
-            all_recs = [r.name for r in study_config.session_def.recordings]
+        all_recs = [r.name for r in study_config.session_def.recordings]
+        for e in trial_events:
             # NB: don't error if we don't need trial episodes for coding.
-            episodes[annotation.EventType.Trial] = synchronization.get_episode_frame_indices_from_ref(working_dir, annotation.EventType.Trial, rec_def.name, study_config.sync_ref_recording, all_recs, study_config.sync_ref_do_time_stretch, study_config.sync_ref_average_recordings, study_config.sync_ref_stretch_which, missing_ref_coding_ok=has_auto_code)
+            episodes[e['name']] = synchronization.get_episode_frame_indices_from_ref(working_dir, e['name'], rec_def.name, study_config.sync_ref_recording, all_recs, study_config.sync_ref_do_time_stretch, study_config.sync_ref_average_recordings, study_config.sync_ref_stretch_which, missing_ref_coding_ok=has_auto_code)
 
-    sync_target_function         = _get_sync_function(study_config, rec_def, None if annotation.EventType.Sync_ET_Data not in episodes else episodes[annotation.EventType.Sync_ET_Data])
-    planes_setup, analyze_frames = _get_plane_setup(study_config, config_dir, episodes)
+    sync_target_functions, function_frames  = _get_sync_function(study_config, rec_def, episodes)
+    planes_setup, plane_frames              = _get_plane_setup(study_config, config_dir, episodes)
 
     # set up pose estimator
     estimator = pose.Estimator(in_video, working_dir / gt_naming.frame_timestamps_fname, working_dir / gt_naming.scene_camera_calibration_fname)
@@ -80,7 +81,7 @@ def do_the_work(working_dir: pathlib.Path, config_dir: pathlib.Path, gui: GUI, v
     # will then wrap their detection and register them with the pose estimator
     aruco_manager = aruco.Manager()
     for p in planes_setup:
-        aruco_manager.add_plane(p, planes_setup[p], analyze_frames[p])
+        aruco_manager.add_plane(p, planes_setup[p], plane_frames[p])
         if hasattr(planes_setup[p]['plane'],'is_dynamic') and planes_setup[p]['plane'].is_dynamic():
             markers = planes_setup[p]['plane'].get_marker_IDs()
             marker_setup = planes_setup[p]['plane'].get_dynamic_marker_setup()
@@ -88,14 +89,14 @@ def do_the_work(working_dir: pathlib.Path, config_dir: pathlib.Path, gui: GUI, v
                 if c=='plane':
                     continue
                 for m in markers[c]:
-                    aruco_manager.add_individual_marker(m, marker_setup, analyze_frames[p])
+                    aruco_manager.add_individual_marker(m, marker_setup, plane_frames[p])
     for m in (markers:=marker.get_setup_for_markers(study_config.individual_markers)):
         aruco_manager.add_individual_marker(m, markers[m])
     aruco_manager.consolidate_setup()
     aruco_manager.register_with_estimator(estimator)
     # other setup of estimator
-    if sync_target_function is not None:
-        estimator.register_extra_processing_fun('sync', *sync_target_function)
+    for sfe in sync_target_functions:
+        estimator.register_extra_processing_fun(f'sync_{sfe}', function_frames[sfe], *sync_target_functions[sfe])
     estimator.attach_gui(gui)
     if gui is not None:
         gui.set_show_timeline(True, timestamps.VideoTimestamps(working_dir / gt_naming.frame_timestamps_fname), annotation.flatten_annotation_dict(episodes), window_id=gui.main_window_id)
@@ -129,58 +130,64 @@ def do_the_work(working_dir: pathlib.Path, config_dir: pathlib.Path, gui: GUI, v
 
 def _get_sync_function(study_config: config.Study,
                        rec_def: session.RecordingDefinition,
-                       episodes: list[list[int]]) -> None | list[Callable[[str,int,np.ndarray,ocv.CameraParams,Any], tuple[float,float]], list[list[int]], dict[str], Callable[[str,int,np.ndarray,int,float,float], None]]:
-    sync_target_function: list[Callable[[str,int,np.ndarray,ocv.CameraParams,Any], tuple[float,float]], list[int]|list[list[int]], dict[str], Callable[[str,int,np.ndarray,int,float,float], None]] = None
+                       episodes: dict[str,list[list[int]]]|None = None) -> tuple[dict[str, tuple[typing.Callable[[str,int,np.ndarray,ocv.CameraParams,typing.Any], tuple[float,float]], dict[str, typing.Any], typing.Callable[[str,np.ndarray,int,tuple[float,float]], None]]], dict[str, list[list[int]]|None]]:
+    sync_target_function: dict[str, tuple[typing.Callable[[str,int,np.ndarray,ocv.CameraParams,typing.Any], tuple[float,float]], dict[str, typing.Any], typing.Callable[[str,np.ndarray,int,tuple[float,float]], None]]] = {}
+    analyze_frames: dict[str, list[list[int]]|None] = {}
+    # NB: only for eye tracker recordings, others don't have eye tracking data and thus nothing to sync
     if rec_def.type==session.RecordingType.Eye_Tracker:
-        # NB: only for eye tracker recordings, others don't have eye tracking data and thus nothing to sync
-        match study_config.get_cam_movement_for_et_sync_method:
-            case '':
-                pass # nothing to do
-            case 'plane':
-                if annotation.EventType.Sync_ET_Data not in study_config.planes_per_episode:
-                    raise ValueError(f'The method for synchronizing eye tracker data to the scene camera (get_cam_movement_for_et_sync_method) is set to "plane" but no plane is configured for {annotation.EventType.Sync_ET_Data.name} in the planes_per_episode config')
-                # NB: no extra_funcs to run
-            case 'function':
-                import importlib
-                to_load = study_config.get_cam_movement_for_et_sync_function['module_or_file']
-                if (to_load_path:=pathlib.Path(to_load)).is_file():
-                    import sys
-                    module_name = to_load_path.stem
-                    spec = importlib.util.spec_from_file_location(module_name, to_load_path)
-                    module = importlib.util.module_from_spec(spec)
-                    sys.modules[module_name] = module
-                    spec.loader.exec_module(module)
-                else:
-                    module = importlib.import_module(study_config.get_cam_movement_for_et_sync_function['module_or_file'])
-                func = getattr(module,study_config.get_cam_movement_for_et_sync_function['function'])
-                sync_target_function = [func, episodes, study_config.get_cam_movement_for_et_sync_function['parameters'], _sync_function_output_drawer]
-            case _:
-                raise ValueError(f'study config get_cam_movement_for_et_sync_method={study_config.get_cam_movement_for_et_sync_method} not understood')
+        et_sync_events = process.get_specific_event_types(study_config, annotation.EventType.Sync_ET_Data, ['sync_setup'])
+        for e in et_sync_events:
+            match e['sync_setup']['get_cam_movement_method']:
+                case 'plane':
+                    if not e['planes']:
+                        raise ValueError(f'The method for synchronizing eye tracker data to the scene camera (get_cam_movement_method) is set to "plane" for the "{e["name"]}" event but no plane is configured for this event. Cannot continue.')
+                    # NB: no extra_funcs to run
+                case 'function':
+                    import importlib
+                    to_load = e['sync_setup']['get_cam_movement_function']['module_or_file']
+                    if (to_load_path:=pathlib.Path(to_load)).is_file():
+                        import sys
+                        module_name = to_load_path.stem
+                        spec = importlib.util.spec_from_file_location(module_name, to_load_path)
+                        module = importlib.util.module_from_spec(spec)
+                        sys.modules[module_name] = module
+                        spec.loader.exec_module(module)
+                    else:
+                        module = importlib.import_module(e['sync_setup']['get_cam_movement_function']['module_or_file'])
+                    func = getattr(module, e['sync_setup']['get_cam_movement_function']['function'])
+                    sync_target_function[e["name"]] = (func, e['sync_setup']['get_cam_movement_function']['parameters'], _sync_function_output_drawer)
+                    if episodes and e['name'] in episodes:
+                        analyze_frames[e['name']] = episodes[e['name']]
+                    else:
+                        analyze_frames[e['name']] = None
+                case _:
+                    raise ValueError(f'sync_setup.get_cam_movement_method={e["sync_setup"]["get_cam_movement_method"]} for the "{e["name"]}" event not understood')
 
-    return sync_target_function
+    return sync_target_function, analyze_frames
 
-def _sync_function_output_drawer(proc_name: str, frame: np.ndarray, frame_idx: int, tx: float, ty: float, sub_pixel_fac=8):
+def _sync_function_output_drawer(proc_name: str, frame: np.ndarray, frame_idx: int, t: tuple[float,float], sub_pixel_fac=8):
     # input is tx, ty pixel positions on the camera image
     ll = 20
+    tx,ty = t
     drawing.openCVLine(frame, (tx,ty-ll), (tx,ty+ll), (0,255,0), 1, sub_pixel_fac)
     drawing.openCVLine(frame, (tx-ll,ty), (tx+ll,ty), (0,255,0), 1, sub_pixel_fac)
     drawing.openCVCircle(frame, (tx,ty), 3, (0,0,255), -1, sub_pixel_fac)
 
 def _get_plane_setup(study_config: config.Study,
                      config_dir: pathlib.Path,
-                     episodes: dict[annotation.EventType,list[list[int]]] = None) -> tuple[dict[str, aruco.PlaneSetup], dict[str, list[list[int]]|None]]:
+                     episodes: dict[str,list[list[int]]]|None = None) -> tuple[dict[str, aruco.PlaneSetup], dict[str, list[list[int]]|None]]:
     # process the above into a dict of plane definitions and a dict with frame number intervals for which to use each
-    planes = {v for k in study_config.planes_per_episode for v in study_config.planes_per_episode[k]}
+    planes = {v for cs in study_config.coding_setup for v in cs['planes']}
     planes_setup: dict[str, aruco.PlaneSetup] = {}
-    analyze_frames: dict[str, list[list[int]]] = {}
+    analyze_frames: dict[str, list[list[int]]|None] = {}
     for p in planes:
         p_def = [pl for pl in study_config.planes if pl.name==p][0]
         pl = plane.get_plane_from_definition(p_def, config_dir/p)
         planes_setup[p] = pl.get_plane_setup()
         if episodes:
             # determine for which frames this plane should be used
-            anal_episodes = [k for k in study_config.planes_per_episode if p in study_config.planes_per_episode[k]]
-            all_episodes = [ep for k in anal_episodes for ep in episodes[k] if ep]  # filter out empty
+            anal_events  = [cs['name'] for cs in study_config.coding_setup if p in cs['planes']]
+            all_episodes = [ep for k in anal_events for ep in episodes[k] if ep]  # filter out empty
             analyze_frames[p] = sorted(all_episodes, key = lambda x: x[1])
         else:
             analyze_frames[p] = None
