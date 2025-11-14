5a735a0e167ffc5865d8568d701b0c07b1cf51db
Diederick Niehorster <dcnieho@gmail.com>
2025-11-14 13:22:25 +0100

    episode: add function to load coding from recording, and add synced coding from any other recording, as available

--- src/gazeMapper/episode.py
+++ src/gazeMapper/episode.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 
 from glassesTools import annotation
 
+from . import config, naming
+
 
 class Episode:
     def __init__(self, event:str, start_frame:int, end_frame:int|None=None, event_type:annotation.EventType|None=None):
@@ -59,6 +61,64 @@ def write_list_to_file(episodes: list[Episode],
     # keep only columns to be written out and order them correctly
     df = df[['event','event_type','start_frame','end_frame']]
     df.to_csv(str(fileName), index=False, sep='\t', na_rep='nan')
+
+def load_episodes_from_all_recordings(study_config: config.Study, recording_dir: str|pathlib.Path, empty_if_no_coding=True, error_if_unwanted_found=True) -> tuple[dict[str, list[list[int]]], set[str]]:
+    from . import synchronization
+    # loads episodes for both the current recording, and from other synced recordings in the session as set up in the study config
+    recording_dir = pathlib.Path(recording_dir)
+    coding_file = recording_dir / naming.coding_file
+    if coding_file.is_file():
+        episodes = list_to_marker_dict(read_list_from_file(coding_file))
+    else:
+        if not empty_if_no_coding:
+            raise FileNotFoundError(f'No coding file found at {coding_file}')
+        episodes = get_empty_marker_dict([])
+
+    # check what coding we expect for this file
+    if len(study_config.session_def.recordings)==1:
+        to_code = {cs['name'] for cs in study_config.coding_setup}
+    else:
+        to_code: set[str] = set()
+        for cs in study_config.coding_setup:
+            if cs['event_type']==annotation.EventType.Sync_Camera:
+                # camera sync events are always coded for all recordings
+                to_code.add(cs['name'])
+                continue
+            which_recs = cs.get('which_recordings')
+            if recording_dir.name in which_recs:
+                to_code.add(cs['name'])
+
+    # add missing fields
+    for evt in to_code:
+        if evt not in episodes:
+            episodes[evt] = []
+
+    # now check if there is coding to get from other recordings, or if there is coding that should not be there
+    rec_name = recording_dir.name
+    # check for unwanted coding
+    for nm in episodes:
+        if episodes[nm] and nm not in to_code:
+            if error_if_unwanted_found:
+                cs = [cs for cs in study_config.coding_setup if cs['name']==nm][0]
+                raise ValueError(f'{nm} episodes are gotten from the recordings {cs.get("which_recordings")} and should not be coded for this recording ({rec_name})')
+            else:
+                del episodes[nm]
+    # check for coding to get from other recordings
+    all_recs = [r.name for r in study_config.session_def.recordings if r.name!=study_config.sync_ref_recording]
+    for cs in study_config.coding_setup:
+        which_recs = cs.get('which_recordings')
+        if which_recs is None or rec_name in which_recs:
+            continue    # this coding is for this recording, so we already have it
+        # get coding from other recordings
+        single_rec = len(which_recs)==1
+        for other_rec in which_recs:
+            # NB: don't error if we don't need trial episodes for coding.
+            extra = '' if single_rec else f' (from recording {other_rec})'
+            eps = synchronization.get_episode_frame_indices_from_other_video(recording_dir, cs['name'], rec_name, other_rec, study_config.sync_ref_recording, all_recs, study_config.sync_ref_do_time_stretch, study_config.sync_ref_average_recordings, study_config.sync_ref_stretch_which, missing_other_coding_ok=True)
+            if eps:
+                episodes[cs['name']+extra] = eps
+
+    return episodes, to_code
 
 
 def get_empty_marker_dict(episodes: list[str]) -> dict[str,list[list[int]]]:
